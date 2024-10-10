aa885c686e35f3a8aa9853f8acb7b108ffd3a044
subpal <subhampal198@gmail.com>
2024-10-10 15:19:53 +0530

    Added meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,6 +20,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Free Background Removal Tool - Remove Image Background Online</title>
+        <meta name="title" content="BG Rem" />
+        <meta name="description" content="Easily remove backgrounds from images with our free online tool. Simple, fast, and accurate background removal for photos and images." />
+
+     
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://bgrem.in/" />
+        <meta property="og:title" content="BG Rem" />
+        <meta property="og:description" content="Easily remove backgrounds from images with our free online tool. Simple, fast, and accurate background removal for photos and images." />
+        <meta property="og:image" content="/bgrem-meta.png" />
+
+       
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://bgrem.in/" />
+        <meta property="twitter:title" content="BG Rem" />
+        <meta property="twitter:description" content="Easily remove backgrounds from images with our free online tool. Simple, fast, and accurate background removal for photos and images." />
+        <meta property="twitter:image" content="bgrem-meta.png" />
+
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} bg-gradient-to-b from-slate-300 to-gray-100`}
       >

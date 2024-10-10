d1362a43d6203f93facb00a3e99d6e91bdb1bf71
subpal <subhampal198@gmail.com>
2024-10-10 14:46:18 +0530

    Added logo

--- src/components/subtitle.tsx
+++ src/components/subtitle.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Subtitle = () => {
   return (
     <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight justify-center flex sm:text-3xl">
-      Remove Background Easily
+      Free Remove Background Easily
     </h2>
   )
 }
--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Title = () => {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center p-1">
+      <Image className="rounded-lg"
+        src="/bgrem.png"
+        width={70}
+        height={70}
+        alt="Logo"
+      />
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl p-3">
         BG Rem
       </h1>

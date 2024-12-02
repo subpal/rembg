4fe0b87d7c9a3a547721cac6636b16d2b6edd845
subpal <subhampal198@gmail.com>
2024-12-02 17:02:17 +0530

    Added icons and animate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Reviews from "@/components/reviews";
 import Subtitle from "@/components/subtitle";
 import {MagicCard} from "@/components/ui/magic-card";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Page() {
   return (
@@ -14,17 +15,22 @@ export default function Page() {
                   }
               >
                   <Link href={"/remove-background"}>
-                      <MagicCard className={"h-56 w-56 items-center justify-center text-xl"}>
-                          Remove background
+                      <MagicCard className={"h-56 w-56 items-center justify-center text-md group hover:bg-yellow-50 hover:text-xl"}>
+                          <div className={"flex flex-col justify-center items-center"}>
+                              <Image className={"m-5 group-hover:animate-bounce"} src={"/background.svg"} alt={"crop image"} width={60} height={60}/>
+                              Remove background
+                          </div>
                       </MagicCard>
                   </Link>
                   <Link href={"/crop"}>
-                      <MagicCard className={"h-56 w-56 items-center justify-center text-xl"}>
-                          Crop Image
+                      <MagicCard className={"h-56 w-56 items-center justify-center text-md group hover:bg-yellow-50 hover:text-xl"}>
+                          <div className={"flex flex-col justify-center items-center"}>
+                              <Image className={"m-5 group-hover:animate-bounce"} src={"/crop.svg"} alt={"crop image"} width={60} height={60}/>
+                              Crop Image
+                          </div>
                       </MagicCard>
                   </Link>
               </div>
-
           <Reviews/>
       </main>
   );
